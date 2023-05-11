e329e8d84d162991a22fb9147f79a15e076391e5
Ross Alexandra <ross-alexandra@outlook.com>
2023-05-10 18:39:40 -0700

    Adds multiprocessing pool to the bulk ip route

--- api/helpers/get_ip_info.py
+++ api/helpers/get_ip_info.py
@@ -1,13 +1,19 @@
 def get_ip_info(reader, ip_address):
-    response = reader.city(ip_address)
+    try: 
+        response = reader.city(ip_address)
 
-    return {
-        'ip_address': ip_address,
-        'country_code': response.country.iso_code,
-        'postal_code': response.postal.code,
-        'city': response.city.name,
-        'time_zone': response.location.time_zone,
-        'latitude': response.location.latitude,
-        'longitude': response.location.longitude,
-        'accuracy_radius': response.location.accuracy_radius,
-    }
+        return {
+            'ip_address': ip_address,
+            'country_code': response.country.iso_code,
+            'postal_code': response.postal.code,
+            'city': response.city.name,
+            'time_zone': response.location.time_zone,
+            'latitude': response.location.latitude,
+            'longitude': response.location.longitude,
+            'accuracy_radius': response.location.accuracy_radius,
+        }
+    
+    except Exception as e:
+        return {
+            'error': f'An error occurred while attempting to retrieve the IP address: {e}'
+        }
--- api/routes/ip.py
+++ api/routes/ip.py
@@ -4,9 +4,5 @@ from api.helpers.get_ip_info import get_ip_info
 
 @app.route('/ip/<ip_address>', methods=['GET'])
 def get_ip(ip_address):
-    try:
-        return get_ip_info(mmdb_reader.reader, ip_address)
-    except Exception as e:
-        return {
-            'error': f'An error occurred while attempting to retrieve the IP address: {e}'
-        }
+    return get_ip_info(mmdb_reader.reader, ip_address)
+
