078b966ebbbb6933eff8ff9138f82b8a31ff6383
Ross Alexandra <ross-alexandra@outlook.com>
2023-05-12 12:36:21 -0700

    Updated backdrop filter

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -37,8 +37,11 @@ export const theme = {
         
         padding: 15px;
         border: 1px solid var(--border-color);
-        backdrop-filter: saturate(180%) blur(20px);
 
         z-index: 10;
+
+        @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+            backdrop-filter: grayscale(1);
+        }
     `,
 };
