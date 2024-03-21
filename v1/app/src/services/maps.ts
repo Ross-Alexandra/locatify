6b4f1ca671a297bb4fc2a37534d1f648f21b662c
Ross Alexandra <ross-alexandra@outlook.com>
2024-03-20 23:03:13 -0700

    Desktop Styling done

--- v1/app/src/services/maps.ts
+++ v1/app/src/services/maps.ts
@@ -1,7 +1,7 @@
 // This key is restricted to Google Maps embed access which
 // is free & unlimited. Thus, it is safe to commit to the repo.
 // https://developers.google.com/maps/documentation/javascript/get-api-key#restrict_key
-const API_KEY = 'AIzaSyDlmsVo1s5SEcxqzVWB2wNnlq3evBV59Jg';
+const API_KEY = 'AIzaSyDbMLwv4De9DG3tX5cyGygTlHMzRYAs-Ng';
 
 export function getMapUrl(lat: number, long: number) {
     return `http://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${lat},${long}&center=${lat},${long}&zoom=12&maptype=roadmap`;
--- v2/src/Controller/HTMXController.php
+++ v2/src/Controller/HTMXController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use \DateTime;
+use \DateTimeZone;
+
 use Psr\Log\LoggerInterface;
 
 use MaxMind\Db\Reader;
@@ -31,10 +34,17 @@ class HTMXController extends AbstractController {
         $reader = new Reader("$home/locatify/database.mmdb");
 
         $ipsToLookup = $request->get('ip');
+        $logger->info('ipsToLookUp: {ips}', [
+            'ips' => print_r($ipsToLookup, true),
+        ]);
         $ipInfo = [];
 
         foreach($ipsToLookup as $lookup) {
-            $ipData = $reader->get($lookup['ip']);
+            $ipData = $reader->get($lookup['ip']) ?? [];
+
+            $logger->info('ipData: {data}', [
+                'data' => print_r($ipData, true)
+            ]);
 
             array_push($ipInfo, [
                 'data' => [
@@ -44,7 +54,8 @@ class HTMXController extends AbstractController {
                     'latitude' => parse_latitude($ipData),
                     'longitude' => parse_longitude($ipData),
                     'accuracy' => parse_accuracy($ipData),
-                    'timezone' => parse_timezone($ipData),
+                    'time' => parse_time($ipData),
+                    'mapLink' => parse_maps_link($ipData, $this->getParameter('app.maps_embed_api_key')),
                 ],
                 'label' => $lookup['label'],
                 'ip' => $lookup['ip'],
@@ -56,7 +67,24 @@ class HTMXController extends AbstractController {
 }
 
 function parse_city($ipData) {
-    return (array_key_exists('city', $ipData) && array_key_exists('name', $ipData['city'])) ? $ipData['city']['name'] : 'Unknown City';
+    if (array_key_exists('city', $ipData) && array_key_exists('name', $ipData['city'])) {
+        return $ipData['city']['name'];
+    } else if (
+        array_key_exists('city', $ipData)
+        && array_key_exists('names', $ipData['city'])
+        && array_key_exists('en', $ipData['city']['names'])
+    ) {
+        return $ipData['city']['names']['en'];
+    } else if (
+        array_key_exists('subdivisions', $ipData)
+        && array_key_exists(0, $ipData['subdivisions'])
+        && array_key_exists('names', $ipData['subdivisions'][0])
+        && array_key_exists('en', $ipData['subdivisions'][0]['names'])
+    ) {
+        return $ipData['subdivisions'][0]['names']['en'];
+    } else {
+        return 'Unknown City';
+    }
 }
 
 function parse_country($ipData) {
@@ -76,9 +104,30 @@ function parse_longitude($ipData) {
 }
 
 function parse_accuracy($ipData) {
-    return (array_key_exists('location', $ipData) && array_key_exists('accuracy_radius', $ipData['location'])) ? $ipData['location']['accuracy_radius'] : INF;
+    return (array_key_exists('location', $ipData) && array_key_exists('accuracy_radius', $ipData['location'])) ? $ipData['location']['accuracy_radius'] : 'Unknown';
 }
 
-function parse_timezone($ipData) {
-    return (array_key_exists('location', $ipData) && array_key_exists('timezone', $ipData['location'])) ? $ipData['location']['timezone'] : 'Unknown';
+function parse_time($ipData) {
+    if (!array_key_exists('location', $ipData) || !array_key_exists('time_zone', $ipData['location'])) {
+        return 'Unknown';
+    }
+
+    $timezone = $ipData['location']['time_zone'];
+    $date = new DateTime('now', new DateTimeZone($timezone));
+    return $date->format('h:i a e(P)');
+}
+
+function parse_maps_link($ipData, $API_KEY) {
+    if (
+        !array_key_exists('location', $ipData)
+        || !array_key_exists('latitude', $ipData['location'])
+        || !array_key_exists('longitude', $ipData['location'])
+    ) {
+        return null;
+    }
+
+    $lat = $ipData['location']['latitude'];
+    $long = $ipData['location']['longitude'];
+
+    return "http://www.google.com/maps/embed/v1/place?key=$API_KEY&q=$lat,$long&center=$lat,$long&zoom=12&maptype=roadmap";
 }
