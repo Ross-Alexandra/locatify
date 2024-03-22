53af0988c8bead5e58735837b1b64312d49a2254
Ross Alexandra <ross-alexandra@outlook.com>
2024-03-21 22:37:56 -0700

    Added a back button to the results page

--- v2/assets/components/manual-search-panel/index.js
+++ v2/assets/components/manual-search-panel/index.js
@@ -1,17 +1,3 @@
-window.addEventListener('load' , () => {
-    const swapButtons = document.querySelectorAll('button.search-type-swap');
-    const searchPanel = document.getElementById('search-panel');
-
-    swapButtons.forEach((button) => {
-        button.addEventListener('click', e => {
-            e.preventDefault();
-
-            const searchType = button.getAttribute('data-swap-to');
-            searchPanel.setAttribute('data-search-type', searchType);
-        });
-    });
-});
-
 function onIpInputInvalid(input) {
     input.setCustomValidity('Please enter a valid IP address');
 }
---
+++ v2/assets/search.js
@@ -0,0 +1,5 @@
+function onClipSwapButtonClick(searchType) {
+    const searchPanel = document.getElementById('search-panel');
+
+    searchPanel.setAttribute('data-search-type', searchType);
+}
