084af5d513ab8c0fe63ef162fe69810010ac80be
Ross Alexandra <ross-alexandra@outlook.com>
2024-03-21 18:00:08 -0700

    Remove unused default controller

--- v2/src/Controller/DefaultController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class DefaultController extends AbstractController {}
