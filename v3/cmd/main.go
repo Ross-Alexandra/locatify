35c3fc66423816604c8893816a9628594dabf4f6
Ross Alexandra <ross-alexandra@outlook.com>
2024-04-11 20:11:55 -0700

    Implements base index page & solves the HTMX problems

--- v3/cmd/main.go
+++ v3/cmd/main.go
@@ -4,8 +4,15 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 type Template struct {
@@ -16,27 +23,59 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type Routing struct {
+	IsHTMX bool
+}
+
+type IndexPage struct {
+	Title         string
+	CopyrightYear int
+	Routing
+}
+
+func getTemplates() []string {
+	templateFiles := make([]string, 0)
+	err := filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() || len(path) < 5 {
+			return nil
+		}
+
+		if path[len(path)-5:] == ".html" {
+			templateFiles = append(templateFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return templateFiles
+}
+
 func main() {
+	templateFiles := getTemplates()
+	println("found template files: ", strings.Join(templateFiles, ", "))
+
 	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+		templates: template.Must(template.ParseFiles(templateFiles...)),
 	}
 
 	e := echo.New()
 	e.Renderer = t
 
+	e.Logger.SetLevel(log.INFO)
+
+	e.Use(middleware.Logger())
+
 	e.Static("/static", "dist")
 	e.Static("/", "public")
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World")
-	})
 
-	e.GET("/index", func(c echo.Context) error {
-		return c.File("index.html")
-	})
-
-	e.GET("/template", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "example.html", nil)
+		return c.Render(http.StatusOK, "search/index", IndexPage{
+			CopyrightYear: time.Now().Year(),
+		})
 	})
 
 	e.Logger.Fatal(e.Start(":34343"))
--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/scripts/index.ts',
+  entry: './src/index.ts',
   resolve: {
     extensions: ['.ts', '.js'],
   },
